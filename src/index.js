9551954025f6fc2a2a1dabedd3d78625e97b7af8
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-10-18 14:14:08 -0400

    Start new app
    Set up a new app and got a few pages running. Pages are:
     - Home Page
     - About Page
     - Contact Page

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './Pages/Home';
+import AboutPage from './Pages/About';
+import ContactPage from './Pages/Contact';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/about",
+    element: <AboutPage />
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />
+  }
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
